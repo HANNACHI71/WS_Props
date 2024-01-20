203504db32b7accd13b033efe138a1d73850b39a
mohamed hannachi <hannachi1717@gmail.com>
2024-01-20 09:28:21 +0100

    WS Props

---
+++ src/players.js
@@ -0,0 +1,43 @@
+const players = [
+    {
+        id : 1,
+        name : "Sadok",
+        team : "Tunisia",
+        nationality :"Tunisian",
+        jerseyNumber : 7,
+        age : 48,
+        image : './image/img1.jpg'
+        },
+        {
+            id : 2,
+            name : "Farah",
+            team : "Tunisia",
+            nationality :"Tunisian",
+            jerseyNumber : 10,
+            age : 23,
+            image : './image/img2.jpg'
+            
+        },    
+        {
+            id : 3,
+            name : "Chaima",
+            team : "Tunisia",
+            nationality :"Tunisian",
+            jerseyNumber : 11,
+            age : 10,
+            image : './image/img3.jpg'
+            
+        },
+        {
+            id : 4,
+            name : "Zohra",
+            team : "Tunisia",
+            nationality :"Tunisian",
+            jerseyNumber : 15,
+            age : 9,
+            image : './image/img4.jpg'
+            
+        }    
+        ]
+
+export default players;
